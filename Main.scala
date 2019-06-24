b0ed3c350e77da96dada1730fe0eba547d2a631a
BinarySoftware <mkmikolajek@icloud.com>
2019-06-24 12:17:47 +0200

    skeleton P-o-C

--- Main.scala
+++ Main.scala
@@ -1,5 +1,14 @@
+import scala.io.Source
+
 object Main {
     def main(args: Array[String]) : Unit = {
-        print("Hello, Stocks")
+        val stocksAPIURL = "https://ws-api.iextrading.com/1.0/tops/last?symbols="
+        val symbols = Array("AAPL","SNAP","FB")
+        val combinedURL = stocksAPIURL.concat(symbols.mkString(","))
+
+        println(combinedURL)
+
+        val stocksData = Source.fromURL(combinedURL).mkString
+        println(stocksData)
     }
 }
