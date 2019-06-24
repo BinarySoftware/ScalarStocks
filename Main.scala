a4478f624c18a62b034ad62d3f5e52eb3ec97a10
BinarySoftware <mkmikolajek@icloud.com>
2019-06-24 12:54:45 +0200

    add appropriate function for getting data

--- Main.scala
+++ Main.scala
@@ -1,14 +1,25 @@
 import scala.io.Source
 
 object Main {
+    def getDataFromURLWithSymbols(urlString: String, symbols: Array[String]) : String = {
+        val stringOfSymbols = symbols.mkString(",")
+        val concatenatedURL = urlString.concat(stringOfSymbols)
+
+        try {
+            Source.fromURL(concatenatedURL).mkString
+        }
+        catch {
+            case _ : Throwable => "An error has occurred"
+        }
+    }
+
     def main(args: Array[String]) : Unit = {
         val stocksAPIURL = "https://ws-api.iextrading.com/1.0/tops/last?symbols="
         val symbols = Array("AAPL","SNAP","FB")
-        val combinedURL = stocksAPIURL.concat(symbols.mkString(","))
+        
 
-        println(combinedURL)
+        val stocksData = getDataFromURLWithSymbols(stocksAPIURL, symbols)
 
-        val stocksData = Source.fromURL(combinedURL).mkString
         println(stocksData)
     }
 }
