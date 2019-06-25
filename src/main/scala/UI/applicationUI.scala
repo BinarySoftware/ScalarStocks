06e1c8a1f8aa4e8596cdd6737b3443158cebdee8
BinarySoftware <mkmikolajek@icloud.com>
2019-06-25 16:16:31 +0200

    Add/Remove is functional now

--- src/main/scala/UI/applicationUI.scala
+++ src/main/scala/UI/applicationUI.scala
@@ -32,16 +32,38 @@ object AppUI {
     }
 
     def makePrompt() {
-        val action = scala.io.StdIn.readLine()
+        val action = scala.io.StdIn.readLine(Console.BLUE + "Command>")
+
         if (action == "R" || action == "r") {
             refreshData(symbols)
         } else if (action == "A" || action == "a") {
-            println(Console.BLUE + "Under construction - add")
+            println(Console.WHITE + "Please type in correct symbol to append")
+            var symbolName = scala.io.StdIn.readLine(Console.BLUE + "Symbol>")
+            symbolName = symbolName.toUpperCase
+
+            if (symbols.contains(symbolName)) {
+                println(Console.RED + "Sorry, your symbols already contain: " +  symbolName)
+                makeControlsForMainScreen()
+            } else {
+                symbols :+= symbolName
+                refreshData(symbols)
+            }
         } else if (action == "D" || action == "d") {
-            println(Console.BLUE + "Under construction - delete")
+            println(Console.WHITE + "Please type in correct symbol to delete")
+            var symbolName = scala.io.StdIn.readLine(Console.BLUE + "Symbol>")
+            symbolName = symbolName.toUpperCase
+
+            if (symbols.contains(symbolName)) {
+                symbols = symbols.filter(! _.contains(symbolName))
+                refreshData(symbols)
+            } else {
+                println(Console.RED + "Sorry, there is no " +  symbolName + " in your symbols array")
+                makeControlsForMainScreen()
+                
+            }
         } else if (action == "Q" || action == "q") {
             println(Console.RED + "Are you sure you want to quit? (Y/N)")
-            val quitAction = scala.io.StdIn.readLine()
+            val quitAction = scala.io.StdIn.readLine(Console.BLUE + "Command>")
 
             if (quitAction == "Y" || quitAction == "y") {
                 System.exit(0)
